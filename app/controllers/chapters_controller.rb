8366e600306346dcb2d70c71adfa7a418e5db84f
Girish Ramesh <rgirish28@gmail.com>
2013-03-27 02:47:12 +0530

    Added first chapter and new chapter option

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,14 +1,20 @@
 class ChaptersController < ApplicationController
   def new
+    @manual = Manual.find(params[:format])
+    @chapter = Chapter.new
   end
 
   def edit
   end
 
   def show
+    @chapter = Chapter.find(params[:id])
   end
 
   def create
+    @manual = Manual.find(params[:format])
+    @manual.chapters.create(params[:chapter])
+    redirect_to @manual
   end
 
   def update
